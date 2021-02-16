b675a2bf937277f6bad76946f6eb57b7cad68c54
BigphilyB <philippe.bloemen@gmail.com>
2021-02-16 13:31:10 +0100

    HTML

---
+++ Pagina3/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home - Brand</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+    <link rel="stylesheet" href="assets/css/Google-Style-Text-Input.css">
+    <link rel="stylesheet" href="assets/css/Header-Blue.css">
+    <link rel="stylesheet" href="assets/css/Navbar.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Button.css">
+</head>
+
+<body>
+    <div>
+        <div class="header-blue" style="height: 600px;">
+            <nav class="navbar navbar-light navbar-expand-md navigation-clean-search" style="padding: 0px;height: 40px;background: #5b5882;text-align: justify;">
+                <div class="container-fluid"><button class="navbar-toggler" data-toggle="collapse"></button>
+                    <nav class="navbar navbar-light navbar-expand-md">
+                        <div class="container-fluid"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                            <div class="collapse navbar-collapse" id="navcol-1">
+                                <ul class="nav navbar-nav ml-auto">
+                                    <li class="nav-item"><a class="nav-link text-right" href="../Pagina1/MainDashboard.php">HOME</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">SHOP</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </nav><img class="img-fluid" src="assets/img/canon-get-inspired-10-tips-festival-photography-5-1920x1080.jpeg" style="width: 1901px;height: 600px;">
+        </div>
+    </div>
+    <section class="features-icons bg-light text-center">
+        <div class="container">
+            <div class="row">
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        <div class="col-xl-8 offset-xl-0 align-self-center mx-auto" style="width: 100%;height: 950px;">
+            <h4 class="text-left" style="border-color: rgb(0,0,0);width: 250px;">Name:</h4><input type="text">
+            <h4 class="text-left" style="border-color: rgb(0,0,0);width: 250px;">Email:</h4><input type="email"><label class="col-sm-2"></label>
+            <h4 class="text-left" style="border-color: rgb(0,0,0);width: 250px;">Phone Number:</h4><input type="tel" style="width: 300px;">
+            <div class="input-group mb-3"></div>
+            <h4 class="text-left" style="border-color: rgb(0,0,0);width: 250px;">Zip Code:</h4><input type="text" style="width: 300px;">
+            <h4 class="text-left" style="border-color: rgb(0,0,0);width: 250px;">Adress:</h4><input type="text" style="width: 300px;">
+            <h4 class="text-left" style="border-color: rgb(0,0,0);width: 250px;">City:</h4><input type="text" style="width: 300px;">
+            <h4 class="text-left" style="border-color: rgb(0,0,0);width: 250px;">Birthday</h4><input type="date" style="width: 300px;">
+            <h4 class="text-left" style="border-color: rgb(0,0,0);width: 250px;">Buy tickets:</h4><input type="number" style="width: 300px;">
+        </div>
+    </section>
+    <footer class="footer bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 my-auto h-100 text-center text-lg-left">
+                    <ul class="list-inline mb-2">
+                        <li class="list-inline-item"><a href="#">About</a></li>
+                        <li class="list-inline-item"><span>⋅</span></li>
+                        <li class="list-inline-item"><a href="#">Contact</a></li>
+                        <li class="list-inline-item"><span>⋅</span></li>
+                        <li class="list-inline-item"><a href="#">Terms of &nbsp;Use</a></li>
+                        <li class="list-inline-item"><span>⋅</span></li>
+                        <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+                    </ul>
+                    <p class="text-muted small mb-4 mb-lg-0">© Brand 2021. All Rights Reserved.</p>
+                </div>
+                <div class="col-lg-6 my-auto h-100 text-center text-lg-right">
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item"><a href="#"></a></li>
+                        <li class="list-inline-item"><a href="#"></a></li>
+                        <li class="list-inline-item"><a href="#"></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/Navbar---Color-Change-on-Scroll.js"></script>
+</body>
+
+</html>
