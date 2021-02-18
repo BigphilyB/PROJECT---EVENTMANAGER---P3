e4e4518cc7f42c60c58889ea181b443f45c44d1d
Jeffrey Vos <200751@student.glu.nl>
2021-02-18 11:30:38 +0100

    Order page working, added ticket function to orderpage and added insert query for client details

--- test.php
+++ test.php
@@ -1,31 +1,72 @@
 <?php
-// if (isset($_POST["bestellen"])) {
     require 'config.php';
-      $currentDate = date("Y-m-d h:i:s");
-      $eventName = $_POST["eventName"];
-      echo $eventName . "<br>";
-      list($eventStartDate) = mysqli_fetch_array($conn->query("SELECT EventStartDate FROM events WHERE EventName = '$eventName';"));
-      list($eventEndDate) = mysqli_fetch_array($conn->query("SELECT EventEndDate FROM events WHERE EventName = '$eventName';"));
-    //   echo "Start Date: " .$eventStartDate . "<br>";
-    //   echo "End Date: " .$eventEndDate . "<br>";
-    //   echo "Current Date: " . $currentDate . "<br>";
-    // }
+
+    $eventName = $_POST["eventName"];
+    $cName = $_POST["name"];
+    $cLastName = $_POST["lastname"];
+    $cEmail = $_POST["email"];
+    $cPhonenumber = $_POST["phonenumber"];
+    $cZipcode = $_POST["zipcode"];
+    $cAdress = $_POST["adress"];
+    $cCity = $_POST["city"];
+    $cDob = $_POST["dob"];
+    $cTickets = $_POST["ticketsBought"];
+    
+  //   if(isset($_POST["submit"])) {
+
+
+  //     if ($conn->connect_error) {
+  //         die("Connection failed: " . $conn->connect_error);
+  //     }
+      
+  //     $sql = "INSERT INTO `clients`(`Name`, `Email`, `PhoneNumber`, `ZipCode`, `Adress`, `City`, `BirthDay`, `BoughtTickets`, `EventName`) VALUES ('$cName', '$cEmail', '$cPhonenumber', '$cZipcode', '$cAdress', '$cCity', '$cDob', '$cTickets', '$eventName')";
+
+  //     if ($conn->query($sql) === TRUE) {
+  //         echo "New record created successfully";
+  //     } else {
+  //         echo "Error: " . $sql . "<br>" . $conn->error;
+  //     }
+
+  //     mysqli_close($conn);
+  // }
+
+    $boolean = true;
+    $currentDate = date("Y-m-d h:i:s");
+    echo $eventName . "<br>";
+    echo $currentDate . "<br>";
+    list($eventStartDate) = mysqli_fetch_array($conn->query("SELECT EventStartDate FROM events WHERE EventName = '$eventName';"));
+    list($eventEndDate) = mysqli_fetch_array($conn->query("SELECT EventEndDate FROM events WHERE EventName = '$eventName';"));
+
     if($currentDate < $eventStartDate){
-        echo "This event hasn't started yet.";
-        echo "<br>";
-    } if($currentDate >= $eventStartDate && $currentDate < $eventEndDate){
+      $boolean = true;
+      echo "This event is not live yet.";
+    } elseif($currentDate >= $eventStartDate && $currentDate < $eventEndDate){
         echo "This event is live right now.";
         echo "<br>";
-    } if($currentDate > $eventEndDate) {
-        echo "This event already happened.";
-        echo "<br>";
+    }  elseif($currentDate >= $eventStartDate && $eventEndDate == NULL){
+        $boolean = false;
+        echo "This event doesn't have an end date.";
+    } else {
+      $boolean = false;
+      echo "This event is no longer live.";
     }
 
     $ticketsBought = $_POST["ticketsBought"];
-    $sql = "UPDATE `events` SET `CurrentTickets`= CurrentTickets - $ticketsBought WHERE `EventName` = '$eventName';";
+    $update = "UPDATE `events` SET `CurrentTickets`= CurrentTickets - $ticketsBought WHERE `EventName` = '$eventName';";
+    $insert = "INSERT INTO `clients`(`Name`, `Email`, `PhoneNumber`, `ZipCode`, `Adress`, `City`, `BirthDay`, `BoughtTickets`, `EventName`) VALUES ('$cName', '$cEmail', '$cPhonenumber', '$cZipcode', '$cAdress', '$cCity', '$cDob', '$cTickets', '$eventName')";
+    
 
-    if ($conn->query($sql) === TRUE) {
-    } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
+    if($boolean){
+      if ($conn->query($update) === TRUE) {
+      } else {
+        echo "Error: " . $update . "<br>" . $conn->error;
+      }
+      if ($conn->query($insert) === TRUE) {
+      } else {
+        echo "Error: " . $insert . "<br>" . $conn->error;
+      }
     }
+    echo $cName . "<br>";
+    echo $cPhonenumber . "<br>";
+    echo $cTickets . "<br>";
 ?>
